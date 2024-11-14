0a8e9f60dea54bf8f4865c8e658565662540c3aa
Sarah Barajas <sarahbarajas91@yahoo.com>
2024-11-13 21:25:10 -0800

    display alert message

---
+++ script.js
@@ -0,0 +1,6 @@
+//add an event listener to the button that triggers once
+document.getElementById('alertButton').addEventListener('click', function handleClick(){
+    //display an alert message when button is clicked 
+    alert( 'Button was clicked');
+    
+}

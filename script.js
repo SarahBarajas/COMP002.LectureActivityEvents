e10c20837007751e15e2565b7294f2577f543985
Sarah Barajas <sarahbarajas91@yahoo.com>
2024-11-13 21:27:01 -0800

    remove event listener

--- script.js
+++ script.js
@@ -2,5 +2,6 @@
 document.getElementById('alertButton').addEventListener('click', function handleClick(){
     //display an alert message when button is clicked 
     alert( 'Button was clicked');
-    
-}
+   // remove the event listener after the first click
+   document.getElementById('alertButton').removeEventListener('click', handleClick);
+});
